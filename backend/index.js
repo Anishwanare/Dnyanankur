824de9386d62e73c0856a3a0624fdc10cbae0af9
Anish Wanare <106481793+Anishwanare@users.noreply.github.com>
2023-10-14 01:55:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,17 @@ app.use(express.json());
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
-    const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 mongoData();
 
+
+// Serve static files from the build directory
+app.use(express.static(path.join(__dirname, "build")));
+
 app.get("/",(req,res)=>{
-    res.send("Hello world")
+    // res.send("Hello world")
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 })
 
 //user Api
