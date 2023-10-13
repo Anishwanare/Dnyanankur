68705246ed5a1d94bc62b74217367d90e34e92ad
Anish Wanare <106481793+Anishwanare@users.noreply.github.com>
2023-10-14 02:04:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,29 @@
-const express = require("express")
-const cors = require("cors")
-const mongoData = require("./db")
-const userRouter = require("./routes/userRoutes")
-
+const express = require("express");
+const cors = require("cors");
+const mongoData = require("./db");
+const userRouter = require("./routes/userRoutes");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "10mb" });
 
 const PORT = process.env.PORT || 3000;
 
 mongoData();
 
-
 // Serve static files from the build directory
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "build"));
 
-app.get("/",(req,res)=>{
-    // res.send("Hello world")
-    res.sendFile(path.join(__dirname, "build", "index.html"));
-})
+// Define a catch-all route to serve the index.html for all non-static routes
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
-//user Api
-app.use("/api",userRouter)
+// User API
+app.use("/api", userRouter);
 
-
-app.listen(PORT , ()=>{
-    console.log("server is running at port " + PORT);
-})
+app.listen(PORT, () => {
+  console.log("Server is running at port " + PORT);
+});
