4ecfbfdbcee65470ffd7a4050546de7602cacab3
Anish Wanare <106481793+Anishwanare@users.noreply.github.com>
2023-10-17 22:40:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.json({ limit: "10mb" });
+// app.use(express.json({ limit: "10mb" });
 
 const PORT = process.env.PORT || 3000;
 
