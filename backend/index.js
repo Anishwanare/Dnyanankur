2c13647e21cbce4223ab61cba6c4319642ec2cc3
Anish Wanare <106481793+Anishwanare@users.noreply.github.com>
2023-10-17 22:51:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 mongoData();
 
