6751d0d660186007f0f6928ba1e64696df194549
Anish Wanare <106481793+Anishwanare@users.noreply.github.com>
2023-10-17 22:44:59 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.json({ limit: "10mb" });
+app.use(express.json({ limit: "10mb" }));
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,7 +16,7 @@ mongoData();
 
 // Define a catch-all route to serve the index.html for all non-static routes
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.send("Hello World");
 });
 
 // User API
