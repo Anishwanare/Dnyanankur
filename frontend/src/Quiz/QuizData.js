db3f7c51648fc8673513e0949a4b56636219e07e
Anish Wanare <106481793+Anishwanare@users.noreply.github.com>
2023-10-27 19:49:46 +0530

    Update QuizData.js

--- frontend/src/Quiz/QuizData.js
+++ frontend/src/Quiz/QuizData.js
@@ -1,102 +1,251 @@
+// const questions1 = [
+//   {
+//     questionText:
+//       "आधुनिक भारताच्या इतिहासामधील खालीलपैकी कोणत्या बाबींचा समावेश भौतिक साधनामध्ये होत नाही ?",
+//     questionOption: [
+//       { answerText: " नाणी", isCorrect: false },
+//       { answerText: "पुतळे", isCorrect: false },
+//       { answerText: " पोवाडे", isCorrect: true },
+//       { answerText: "वास्तू", isCorrect: false },
+//     ],
+//   },
+//   {
+//     questionText: " सेल्यूलर जेल हे कोठे आहे ?",
+//     questionOption: [
+//       { answerText: "कोलकत्ता", isCorrect: false },
+//       { answerText: "अंदमान", isCorrect: true },
+//       { answerText: "पुणे", isCorrect: false },
+//       { answerText: "वर्धा", isCorrect: false },
+//     ],
+//   },
+//   {
+//     questionText:
+//       "विष्णूशास्त्री चिपळूणकर यांनी कोणत्या मासिकातून सामाजिक व सांस्कृतिक विषयांवर भाष्य केले ?",
+//     questionOption: [
+//       { answerText: "मुकनायक", isCorrect: false },
+//       { answerText: "प्रभाकर", isCorrect: false },
+//       { answerText: "जनता", isCorrect: false },
+//       { answerText: "निबंधमाला", isCorrect: true },
+//     ],
+//   },
+//   {
+//     questionText:
+//       "लोकहितवादी ऊर्फ गोपाळ हरी देशमुख यांनी कोणत्या साप्ताहिकातून शतपत्रे लिहीली ?",
+//     questionOption: [
+//       { answerText: "मुकनायक", isCorrect: false },
+//       { answerText: "प्रभाकर", isCorrect: true },
+//       { answerText: "जनता", isCorrect: false },
+//       { answerText: "निबंधमाला", isCorrect: false },
+//     ],
+//   },
+//   {
+//     questionText: "आगाखान पॅलेस हे कोठे आहे ?",
+//     questionOption: [
+//       { answerText: "मुंबई       ", isCorrect: false },
+//       { answerText: "औरंगाबाद        ", isCorrect: false },
+//       { answerText: "वर्धा             ", isCorrect: false },
+//       { answerText: "पुणे", isCorrect: true },
+//     ],
+//   },
+//   {
+//     questionText:
+//       "डॉ. बाबासाहेब आंबेडकरांनी 1920 च्या जानेवारी महिण्यात कोणते पाक्षिक सुरु केले ?",
+//     questionOption: [
+//       { answerText: "बहिष्कृत भारत", isCorrect: false },
+//       { answerText: " जनता", isCorrect: false },
+//       { answerText: "मुकनायक ", isCorrect: true },
+//       { answerText: "प्रबुध्द भारत", isCorrect: false },
+//     ],
+//   },
+//   {
+//     questionText:
+//       "डॉ. बाबासाहेब आंबेडकरांनी कोणत्या साली बहिष्कृत भारत हे पत्र सुरु केले ?",
+//     questionOption: [
+//       { answerText: "इ.स. 1920   ", isCorrect: false },
+//       { answerText: " इ.स. 1927    ", isCorrect: true },
+//       { answerText: "इ.स. 1919        ", isCorrect: false },
+//       { answerText: "इ.स. 1930", isCorrect: false },
+//     ],
+//   },
+//   {
+//     questionText:
+//       "आपणास शहराचे किंवा एखादया ठिकाणाचे स्वरुप कशावरुन अभ्यासता येते?",
+//     questionOption: [
+//       { answerText: "पोवाडे ", isCorrect: false },
+//       { answerText: " नकाशा", isCorrect: true },
+//       { answerText: "पुस्तके", isCorrect: false },
+//       { answerText: "वृत्तपत्रे", isCorrect: false },
+//     ],
+//   },
+//   {
+//     questionText:
+//       "आधुनिक भारताच्या इतिहासाच्या मौखिक साधनामध्ये खालीलपैकी कोणत्या साधनांचासमावेश होतो ?",
+//     questionOption: [
+//       { answerText: "पोवाडे ", isCorrect: true },
+//       { answerText: " वृत्तपत्रे", isCorrect: false },
+//       { answerText: "वास्तू", isCorrect: false },
+//       { answerText: "पुस्तके", isCorrect: false },
+//     ],
+//   },
+//   {
+//     questionText:
+//       "ब्रिटीश काळात कोणता स्वतंत्र्य विभाग स्थापन करण्यात आला होता ?",
+//     questionOption: [
+//       { answerText: "गेट वे ऑफ इंडिया ", isCorrect: false },
+//       { answerText: " सव्र्व्हे ऑफ इंग्लंड", isCorrect: false },
+//       { answerText: "मुंबई पोर्ट ट्रस्ट", isCorrect: false },
+//       { answerText: "सर्व्हे ऑफ इंडिया", isCorrect: true },
+//     ],
+//   },
+// ];
+
 const questions = [
   {
-    questionText:
-      "आधुनिक भारताच्या इतिहासामधील खालीलपैकी कोणत्या बाबींचा समावेश भौतिक साधनामध्ये होत नाही ?",
+    questionText: "Milky Way Is __ Galaxy.",
     questionOption: [
-      { answerText: " नाणी", isCorrect: false },
-      { answerText: "पुतळे", isCorrect: false },
-      { answerText: " पोवाडे", isCorrect: true },
-      { answerText: "वास्तू", isCorrect: false },
+      { answerText: "Spiral", isCorrect: true },
+      { answerText: "Parellel", isCorrect: false },
+      { answerText: "Equal", isCorrect: false },
+      { answerText: "All Of These", isCorrect: false },
     ],
   },
   {
-    questionText: " सेल्यूलर जेल हे कोठे आहे ?",
+    questionText: "There Are About __ Stors In Our Galaxy.",
     questionOption: [
-      { answerText: "कोलकत्ता", isCorrect: false },
-      { answerText: "अंदमान", isCorrect: true },
-      { answerText: "पुणे", isCorrect: false },
-      { answerText: "वर्धा", isCorrect: false },
+      { answerText: "10⁵", isCorrect: false },
+      { answerText: "10¹⁰", isCorrect: false },
+      { answerText: "10¹¹", isCorrect: true },
+      { answerText: "10¹⁵", isCorrect: false },
     ],
   },
   {
-    questionText:
-      "विष्णूशास्त्री चिपळूणकर यांनी कोणत्या मासिकातून सामाजिक व सांस्कृतिक विषयांवर भाष्य केले ?",
+    questionText: "Surface Temperature Of The Sun Is __.",
     questionOption: [
-      { answerText: "मुकनायक", isCorrect: false },
-      { answerText: "प्रभाकर", isCorrect: false },
-      { answerText: "जनता", isCorrect: false },
-      { answerText: "निबंधमाला", isCorrect: true },
-    ],
-  },
-  {
-    questionText:
-      "लोकहितवादी ऊर्फ गोपाळ हरी देशमुख यांनी कोणत्या साप्ताहिकातून शतपत्रे लिहीली ?",
-    questionOption: [
-      { answerText: "मुकनायक", isCorrect: false },
-      { answerText: "प्रभाकर", isCorrect: true },
-      { answerText: "जनता", isCorrect: false },
-      { answerText: "निबंधमाला", isCorrect: false },
-    ],
-  },
-  {
-    questionText: "आगाखान पॅलेस हे कोठे आहे ?",
-    questionOption: [
-      { answerText: "मुंबई       ", isCorrect: false },
-      { answerText: "औरंगाबाद        ", isCorrect: false },
-      { answerText: "वर्धा             ", isCorrect: false },
-      { answerText: "पुणे", isCorrect: true },
+      { answerText: "6000k", isCorrect: false },
+      { answerText: "5500k", isCorrect: false },
+      { answerText: "5800k", isCorrect: true },
+      { answerText: "5200k", isCorrect: false },
     ],
   },
   {
     questionText:
-      "डॉ. बाबासाहेब आंबेडकरांनी 1920 च्या जानेवारी महिण्यात कोणते पाक्षिक सुरु केले ?",
+      "The Masses Of Other Stars Are Measured Relative To The Mass Of __.",
     questionOption: [
-      { answerText: "बहिष्कृत भारत", isCorrect: false },
-      { answerText: " जनता", isCorrect: false },
-      { answerText: "मुकनायक ", isCorrect: true },
-      { answerText: "प्रबुध्द भारत", isCorrect: false },
+      { answerText: "Earth", isCorrect: false },
+      { answerText: "Sun", isCorrect: true },
+      { answerText: "Moon", isCorrect: false },
+      { answerText: "Jupiter", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "For Measuring Large Distance __ Is Used As A Unit.",
+    questionOption: [
+      { answerText: "Kilometres", isCorrect: false },
+      { answerText: "Light Year", isCorrect: true },
+      { answerText: "Picometer", isCorrect: false },
+      { answerText: "Centimetre", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "The Element Which Constitute 26% Of The Mass Of The Sun .",
+    questionOption: [
+      { answerText: "Hydrogen", isCorrect: false },
+      { answerText: "Helium", isCorrect: true },
+      { answerText: "Carbon", isCorrect: false },
+      { answerText: "Nitrogen", isCorrect: false },
     ],
   },
   {
     questionText:
-      "डॉ. बाबासाहेब आंबेडकरांनी कोणत्या साली बहिष्कृत भारत हे पत्र सुरु केले ?",
+      "The Element Acting As A Fuel In Energy Generation In The Sun .",
     questionOption: [
-      { answerText: "इ.स. 1920   ", isCorrect: false },
-      { answerText: " इ.स. 1927    ", isCorrect: true },
-      { answerText: "इ.स. 1919        ", isCorrect: false },
-      { answerText: "इ.स. 1930", isCorrect: false },
+      { answerText: "Hydrogen", isCorrect: true },
+      { answerText: "Helium", isCorrect: false },
+      { answerText: "Sulphur", isCorrect: false },
+      { answerText: "Boron", isCorrect: false },
     ],
   },
   {
     questionText:
-      "आपणास शहराचे किंवा एखादया ठिकाणाचे स्वरुप कशावरुन अभ्यासता येते?",
+      "Life Span Of Our Sun From Birth Till It Becomes Red Giant Is Of __.",
     questionOption: [
-      { answerText: "पोवाडे ", isCorrect: false },
-      { answerText: " नकाशा", isCorrect: true },
-      { answerText: "पुस्तके", isCorrect: false },
-      { answerText: "वृत्तपत्रे", isCorrect: false },
+      { answerText: "4.5 × 10⁹ To 5 × 10⁹ Years", isCorrect: false },
+      { answerText: "8.5 × 10⁹ To 9.5 × 10⁹ Years", isCorrect: true },
+      { answerText: "4.5 Billion Years", isCorrect: false },
+      { answerText: "4.5 Million Years", isCorrect: false },
     ],
   },
   {
-    questionText:
-      "आधुनिक भारताच्या इतिहासाच्या मौखिक साधनामध्ये खालीलपैकी कोणत्या साधनांचासमावेश होतो ?",
+    questionText: "How Much Radius Of Sun",
     questionOption: [
-      { answerText: "पोवाडे ", isCorrect: true },
-      { answerText: " वृत्तपत्रे", isCorrect: false },
-      { answerText: "वास्तू", isCorrect: false },
-      { answerText: "पुस्तके", isCorrect: false },
+      { answerText: "695700km", isCorrect: true },
+      { answerText: "620027km", isCorrect: false },
+      { answerText: "97243km", isCorrect: false },
+      { answerText: "699923km", isCorrect: false },
     ],
   },
   {
-    questionText:
-      "ब्रिटीश काळात कोणता स्वतंत्र्य विभाग स्थापन करण्यात आला होता ?",
+    questionText: "Stars Are Spheres Of __ Gas.",
     questionOption: [
-      { answerText: "गेट वे ऑफ इंडिया ", isCorrect: false },
-      { answerText: " सव्र्व्हे ऑफ इंग्लंड", isCorrect: false },
-      { answerText: "मुंबई पोर्ट ट्रस्ट", isCorrect: false },
-      { answerText: "सर्व्हे ऑफ इंडिया", isCorrect: true },
+      { answerText: "Hot", isCorrect: true },
+      { answerText: "Cold", isCorrect: false },
+      { answerText: "Normal", isCorrect: false },
+      { answerText: "Both", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "The Solar System Is Situated At A Distance Of __.",
+    questionOption: [
+      { answerText: "3×10⁸ km From Centre Of Milky Way", isCorrect: false },
+      {
+        answerText: "12×10¹⁶ km From Periphery Of Milky Way",
+        isCorrect: false,
+      },
+      { answerText: "2×10¹⁷ km From Centre Of Milky Way", isCorrect: true },
+      {
+        answerText: "2 Light Years From Periphery Of Milky Way",
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    questionText: "The End Stage Of The Sun Will Be __.",
+    questionOption: [
+      { answerText: "Mass", isCorrect: false },
+      { answerText: "White Dwarf", isCorrect: true },
+      { answerText: "Black Dwarf", isCorrect: false },
+      { answerText: "Black Hole", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "The Large The Mass Of A Star The Faster Is its __.",
+    questionOption: [
+      { answerText: "Rate Of Evolution", isCorrect: true },
+      { answerText: "Rate Of Speed", isCorrect: false },
+      { answerText: "Light Years", isCorrect: false },
+      { answerText: "All Of These", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Speed Of The Light Is __.",
+    questionOption: [
+      { answerText: "1,00,000", isCorrect: false },
+      { answerText: "2,00,000", isCorrect: false },
+      { answerText: "3,00,000", isCorrect: true },
+      { answerText: "4,00,000", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "For Detection Of Various Start Used __ Telescope.",
+    questionOption: [
+      { answerText: "Hubble", isCorrect: true },
+      { answerText: "Electron", isCorrect: false },
+      { answerText: "Photometer", isCorrect: false },
+      { answerText: "Compound", isCorrect: false },
     ],
   },
 ];
 
-export default questions;
+
+export default questions ;
+
